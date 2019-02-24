e7e5ee89eb0eec804a2ceef8ef8f92c2d1054369
JM Suchier <jmsuchier@free.fr>
2019-02-24 12:49:58 +0100

    First commit

---
+++ StationsSki/Controleurs/DepartViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DepartViewController.swift
+//  StationsSki
+//
+//  Created by Jean-Marc on 24/02/2019.
+//  Copyright © 2019 Suchier Jean-Marc. All rights reserved.
+//
+
+import UIKit
+
+class DepartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func favorite(_ sender: Any) {
+        print("favorite")
+    }
+    
+    @IBAction func liste(_ sender: Any) {
+        print("Liste")
+    }
+   
+
+}
